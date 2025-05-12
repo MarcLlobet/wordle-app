cc63d7ddf63d87a4b75b2fbd99d45ad4006cfb08
Marc Llobet <Tofu>
2025-05-12 21:15:44 +0200

    vite base config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
+import { defineConfig, ViteUserConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-})
+    base: '/millor-primera-paraula-wordle/',
+} as ViteUserConfig)
