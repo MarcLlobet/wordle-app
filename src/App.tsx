35adf780625cc8300deae8c1de21faa2dde6d230
Marc Llobet <Tofu>
2025-05-12 20:56:48 +0200

    ci secrets && clean fe

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,14 @@
-import { useEffect, useState } from 'react'
+import React from 'react'
 import './App.css'
 
-const getGreeting = async function () {
-    const res = await fetch(
-        'https://wordle-app-33d0a5d98822.herokuapp.com/api',
-        {
-            method: 'GET',
-            mode: 'no-cors',
-        }
-    )
-    return await res.json()
-}
-
 function App() {
-    const [millorEntrada, setMillorEntrada] = useState('')
-
-    useEffect(() => {
-        getGreeting().then((res) => setMillorEntrada(res.millorEntrada?.origen))
-    }, [])
-
     return (
         <>
             <p>
-                Millor primera paraula: <span>{millorEntrada}</span>
+                Millor primera paraula: <span>{1}</span>
             </p>
         </>
     )
 }
 
-export default App
+export default React.memo(App)
