349aab007cc37256005ef35c75530c397abf0657
Marc Llobet <Tofu>
2025-05-12 22:21:55 +0200

    server add cors support

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,12 @@
 import 'dotenv/config'
 import express from 'express'
+import cors from 'cors'
 import { getPrimeraParaula } from './db/queries/getPrimeraParaula'
 import { getSeguentParaula } from './db/queries/getSeguentParaula'
 
 export const app = express()
 app.use(express.json())
+app.use(cors())
 
 const PORT = process.env.PORT ?? 3000
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 
+const getMillorPrimeraParaula = async () => {
+    const response = await fetch(
+        'https://wordle-app-d8u0.onrender.com/api/primera-paraula?llargada=5'
+    )
+    const json = await response.json()
+    return json?.millorParaula?.paraula_original
+}
+
 function App() {
+    const [millorParaula, setMillorParaula] = useState('')
+    useEffect(() => {
+        ;(async () => {
+            const millorParaula = await getMillorPrimeraParaula()
+            setMillorParaula(millorParaula)
+        })()
+    }, [])
     return (
         <>
             <p>
-                Millor primera paraula: <span>{1}</span>
+                Millor primera paraula: <span>{millorParaula}</span>
             </p>
         </>
     )
